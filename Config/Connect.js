4835f31d4ceea4ea7c18da9a050563e4774ed65b
giahao1609 <hoanghao20031609@gmail.com>
2025-03-12 19:45:34 +0700

    init config

--- Config/Connect.js
+++ Config/Connect.js
@@ -1,6 +1,7 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
-const uri = "mongodb+srv://hoanggiahao:Giahao16%40@giahao.3xcgm.mongodb.net/?retryWrites=true&w=majority&appName=Giahao";
+const uri =
+  "mongodb+srv://hoanggiahao:Giahao16%40@giahao.3xcgm.mongodb.net/?retryWrites=true&w=majority&appName=Giahao";
 
 // Tạo MongoClient với các tùy chọn
 const client = new MongoClient(uri, {
@@ -8,7 +9,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 // Hàm kết nối MongoDB
@@ -18,11 +19,13 @@ async function connectDB() {
     await client.connect();
     // Gửi lệnh ping để xác nhận kết nối
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-    return true;  // Kết nối thành công
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
+    return true; // Kết nối thành công
   } catch (err) {
     console.error("Lỗi kết nối MongoDB:", err);
-    return false;  // Kết nối thất bại
+    return false; // Kết nối thất bại
   }
 }
 
